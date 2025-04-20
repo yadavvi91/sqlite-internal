48a097ee72ec0b0209e02ea42bf802dc0f0c1588
Visal In <invisal@gmail.com>
2025-04-20 10:27:08 +0700

    fixing the build error

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { useState } from "react";
 import Viewer from "./Viewer";
 import FileDropZone from "./components/file-drop-zone";
 import { InfoProvider } from "./components/info";
